1f8e0149eef6585bd14b06821b576126a6c754af
Amitesh Sahay <amitesh.sahay@hotmail.com>
2021-04-05 11:43:02 +0530

    modified create and validate functions

--- EmployeePortal/AUTHENTICATION/serializers.py
+++ EmployeePortal/AUTHENTICATION/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import NewEmployeeProfile
+from django.contrib.auth.hashers import make_password
 
 
 class RegistrationSerializers(serializers.ModelSerializer):
@@ -15,25 +16,38 @@ class RegistrationSerializers(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
+    # def create(self, validated_data):
+    #     """
+    #     before we save the new user, we need to make sure that the password1, and password2 matches. In order to do
+    #     that, we need to override the save() method.
+    #     """
+    #
+    #     password = self.validated_data['password']
+    #     password2 = self.validated_data['password2']
+    #
+    #     if password != password2:
+    #         raise serializers.ValidationError({'password': f'password must match..'})
+    #         return
+    #         #return NewEmployeeProfile.objects.create(**validated_data)
+    #
+    # def validate(self, attrs):
+    #     if attrs:
+    #         account = NewEmployeeProfile(
+    #             email=self.validated_data['email'],
+    #             first_name=self.validated_data['first_name'],
+    #             last_name=self.validated_data['last_name'],
+    #             employee_code=self.validated_data['employee_code'],
+    #             contact=self.validated_data['contact'],
+    #             dob=self.validated_data['dob'],
+    #         )
+    #         account.save()
+    #         return account
+
     def create(self, validated_data):
-        """
-        before we save the new user, we need to make sure that the password1, and password2 matches. In order to do
-        that, we need to override the save() method.
-        """
-        account = NewEmployeeProfile(
-            email=self.validated_data['email'],
-            first_name=self.validated_data['first name'],
-            last_name=self.validated_data['last name'],
-            employee_code=self.validated_data['employee code'],
-            contact=self.validated_data['contact'],
-            dob=self.validated_data['dob'],
-        )
+        validated_data["password"] = make_password(validated_data["password"])
+        super().create(validated_data)
 
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
-
-        if password != password2:
-            raise serializers.ValidationError({'password': f'password must match..'})
-        account.set_password(password)
-        account.save()
-        return account
+    def validate(self, attrs):
+        if attrs.get("password") != attrs.pop("password2"):
+            raise serializers.ValidationError({"password": f"password must match.."})
+        return super().validate(attrs)
