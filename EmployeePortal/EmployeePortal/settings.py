c073636d799bbff1249ff4a42a5cc7067a6d71c1
SuperGoogler <54936252+SuperGoogler@users.noreply.github.com>
2021-04-05 18:46:55 +0530

    Update settings.py

--- EmployeePortal/EmployeePortal/settings.py
+++ EmployeePortal/EmployeePortal/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Employee_System',
-        'USER': 'postgres',
-        'PASSWORD': 'Luren(098',
+        'USER': ''***'',
+        'PASSWORD': ''***'',
         'HOST': 'localhost',
         'PORT': '5432',
     }
