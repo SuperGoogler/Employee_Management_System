8c3192103570259689ee19a955ff2e23b5687c92
SuperGoogler <54936252+SuperGoogler@users.noreply.github.com>
2021-04-08 15:25:43 +0530

    Update views.py

--- EmployeePortal/AUTHENTICATION/views.py
+++ EmployeePortal/AUTHENTICATION/views.py
@@ -30,15 +30,25 @@ def signupView(request):
 class UserRegisterView(ListCreateAPIView):
     create_queryset = NewEmployeeProfile.objects.all()
     serializer_class = RegistrationSerializers
-    permission_classes = [IsAuthenticated, IsAdminUser]
-    #permission_classes = [AllowAny]
+    # permission_classes = [IsAuthenticated, IsAdminUser]
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = RegistrationSerializers(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.errors
-            newUser = serializer.save()
-            serializer = RegistrationSerializers(newUser)
-            return Response(data={"status": "OK", "message": serializer.data}, status=status.HTTP_201_CREATED)
-        return Response(data={"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        newUser = serializer.save()
+        serializer = RegistrationSerializers(newUser)
+        return Response(data={"status": "OK", "message": serializer.data, "token": AuthToken.objects.create(newUser)[1]}
+                        , status=status.HTTP_201_CREATED)
+
+
+class loginAPIView(KLView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(loginAPIView, self).post(request, format=None)
 
